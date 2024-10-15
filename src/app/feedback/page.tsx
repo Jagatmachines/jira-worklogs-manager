86bbf287177ef61627d9d508cf5c9df694cc9d37
Jagatjyoti Tuladhar <jagatjyoti@fusemachines.com>
2024-10-15 16:47:36 +0545

    resolve build issue of feedback form

--- src/app/feedback/page.tsx
+++ src/app/feedback/page.tsx
@@ -1,6 +1,6 @@
 const FeedbackPage = () => {
 	return (
-		<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSe8XG4Q4rNjQsMAwm0U61WpTWjIRPNfcsj3gv7yw8OdI8iNeA/viewform?embedded=true" width="640" height="878" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+		<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSe8XG4Q4rNjQsMAwm0U61WpTWjIRPNfcsj3gv7yw8OdI8iNeA/viewform?embedded=true" width="640" height="878" frameBorder="0" marginHeight={0} marginWidth={0}>Loading…</iframe>
 	);
 };
 export default FeedbackPage;
